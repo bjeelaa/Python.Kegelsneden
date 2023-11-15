558457f31bd12b1278aed8371ac9481d8d8d4c0f
bjeelaa <belahoutman@gmail.com>
2023-11-15 21:04:40 +0100

    classificatie

--- functions.py
+++ functions.py
@@ -5,3 +5,6 @@ def det2x2(A):
 def det3x3(A):
     det = A[0][0]*A[1][1]*A[2][2] + A[0][2]*A[1][0]*A[2][1] + A[0][1]*A[1][2]*A[2][0] - A[0][2]*A[1][1]*A[2][0] - A[0][1]*A[1][0]*A[2][2] - A[0][0]*A[1][2]*A[2][1]
     return det
+
+def wegMetDie_xy_term(A):
+    pass
--- main.py
+++ main.py
@@ -15,6 +15,40 @@ A[2][1] = A[1][2]
 d = det2x2([[A[0][0], A[0][1]], [A[1][0], A[1][1]]])
 D = det3x3(A)
 
-print(A)
+print(A[0])
+print(A[1])
+print(A[2])
 print(d)
 print(D)
+
+aard = 0
+alleAarden = [
+    "error",
+    "reële ellips",
+    "imaginaire ellips",
+    "ontaarde ellips",
+    "reële hyperbool",
+    "ontaarde hyperbool",
+    "reële parabool",
+    "ontaarde parabool van 2 evenwijdige reële rechten",
+    "ontaarde parabool van 2 evenwijdige toegevoeg imaginaire rechten",
+    "ontaarde parabool van 2 samenvallende rechten"
+]
+
+if d > 0: #ellips
+    if A[0][0] * D < 0: aard = 1
+    elif A[0][0] * D > 0: aard = 2
+    elif D == 0: aard = 3
+elif d < 0: #hyperbool
+    if D != 0: aard = 4
+    elif D == 0: aard = 5
+elif d == 0: #parabool
+    if D != 0: aard = 6
+    elif D == 0: #ontaarde parabool
+        if D > 0: aard = 7
+        elif D < 0: aard = 8
+        elif D == 0: aard = 9
+
+print(alleAarden[aard])
+
+wegMetDie_xy_term(A)
