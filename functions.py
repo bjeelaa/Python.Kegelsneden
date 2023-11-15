3fbb57a6ffbb0f6d8c8de2bab0dca7e815d62fd8
bjeelaa <belahoutman@gmail.com>
2023-11-15 21:45:04 +0100

    elegantere code

--- functions.py
+++ functions.py
@@ -6,5 +6,22 @@ def det3x3(A):
     det = A[0][0]*A[1][1]*A[2][2] + A[0][2]*A[1][0]*A[2][1] + A[0][1]*A[1][2]*A[2][0] - A[0][2]*A[1][1]*A[2][0] - A[0][1]*A[1][0]*A[2][2] - A[0][0]*A[1][2]*A[2][1]
     return det
 
-def wegMetDie_xy_term(A):
+def classificeer(d,D):
+    aard = 0
+    if d > 0: #ellips
+        if A[0][0] * D < 0: aard = 1
+        elif A[0][0] * D > 0: aard = 2
+        elif D == 0: aard = 3
+    elif d < 0: #hyperbool
+        if D != 0: aard = 4
+        elif D == 0: aard = 5
+    elif d == 0: #parabool
+        if D != 0: aard = 6
+        elif D == 0: #ontaarde parabool
+            if D > 0: aard = 7
+            elif D < 0: aard = 8
+            elif D == 0: aard = 9
+    return aard
+
+def xyWegwerken(A):
     pass
--- main.py
+++ main.py
@@ -5,11 +5,11 @@ A = [[0,0,0],[0,0,0],[0,0,0]]
 A[0][0] = int(input('Coëfficient bij x^2? '))
 A[1][1] = int(input('Coëfficient bij y^2? '))
 A[2][2] = int(input('Coëfficient bij z^2? '))
-A[0][1] = int(input('Coëfficient bij xy? '))/2
+A[0][1] = int(input('Coëfficient bij xy?  '))/2
 A[1][0] = A[0][1]
-A[0][2] = int(input('Coëfficient bij xz? '))/2
+A[0][2] = int(input('Coëfficient bij xz?  '))/2
 A[2][0] = A[0][2]
-A[1][2] = int(input('Coëfficient bij yz? '))/2
+A[1][2] = int(input('Coëfficient bij yz?  '))/2
 A[2][1] = A[1][2]
 
 d = det2x2([[A[0][0], A[0][1]], [A[1][0], A[1][1]]])
@@ -22,33 +22,22 @@ print(d)
 print(D)
 
 aard = 0
-alleAarden = [
-    "error",
-    "reële ellips",                                                             # x^2 + y^2 = 1
-    "imaginaire ellips",                                                        # x^2 + y^2 = -1
-    "ontaarde ellips",                                                          # x^2 + y^2 = 0
-    "reële hyperbool",                                                          # x^2 - y^2 = 1
-    "ontaarde hyperbool",                                                       # x^2 - y^2 = 0
-    "reële parabool",                                                           # y = x^2
-    "ontaarde parabool van 2 evenwijdige reële rechten",                        # x^2 = 1
-    "ontaarde parabool van 2 evenwijdige toegevoeg imaginaire rechten",         # x^2 = -1
-    "ontaarde parabool van 2 samenvallende rechten"                             # x^2 = 0
+aarden = [
+    "error",                                                                    #! what the fuck
+    "reële ellips",                                                             #* x^2 + y^2 = 1
+    "imaginaire ellips",                                                        #* x^2 + y^2 = -1
+    "ontaarde ellips",                                                          #* x^2 + y^2 = 0
+    "reële hyperbool",                                                          #* x^2 - y^2 = 1
+    "ontaarde hyperbool",                                                       #* x^2 - y^2 = 0
+    "reële parabool",                                                           #* y = x^2
+    "ontaarde parabool van 2 evenwijdige reële rechten",                        #* x^2 = 1
+    "ontaarde parabool van 2 evenwijdige toegevoeg imaginaire rechten",         #* x^2 = -1
+    "ontaarde parabool van 2 samenvallende rechten"                             #* x^2 = 0
 ]
 
-if d > 0: #ellips
-    if A[0][0] * D < 0: aard = 1
-    elif A[0][0] * D > 0: aard = 2
-    elif D == 0: aard = 3
-elif d < 0: #hyperbool
-    if D != 0: aard = 4
-    elif D == 0: aard = 5
-elif d == 0: #parabool
-    if D != 0: aard = 6
-    elif D == 0: #ontaarde parabool
-        if D > 0: aard = 7
-        elif D < 0: aard = 8
-        elif D == 0: aard = 9
+#TODO gebruik later de aard als controle!
+aard = classificeer(d,D)
 
-print(alleAarden[aard])
+print(aarden[aard])
 
-if A[0][1] != 0: A = wegMetDie_xy_term(A)
+if A[0][1] != 0: A = xyWegwerken(A)
